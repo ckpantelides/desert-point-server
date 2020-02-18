7d6f3d0f22eb443d5f4ea719c6570c868b9c1295
CK Pantelides <ckpantelides@gmail.com>
2020-02-18 20:52:50 +0000

    Added localhost header exception

--- index.js
+++ index.js
@@ -20,8 +20,17 @@ app.use(function(req, res, next) {
   next();
 });
 */
-app.use(cors());
-app.options("*", cors());
+//app.use(cors());
+//app.options("*", cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
