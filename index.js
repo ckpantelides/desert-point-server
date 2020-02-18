0372319311eab5540004ba3ceb6a485f9673cf14
CK Pantelides <ckpantelides@gmail.com>
2020-02-18 20:28:57 +0000

    Update to header - added localhost

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Methods", "POST, OPTIONS");
   next();
 });
 
