33c93e3b3878aaa9414583fb81efc314a783f29a
CK Pantelides <ckpantelides@gmail.com>
2020-02-20 17:57:58 +0000

    Uninstalled server & sqlite3, removed commented out header code

--- index.js
+++ index.js
@@ -8,20 +8,7 @@ const bodyParser = require('body-parser');
 
 // CORS middleware allows data to be sent from client-side
 const cors = require('cors');
-
 app.use(cors());
-//app.options("*", cors());
-
-/*
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
-*/
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
