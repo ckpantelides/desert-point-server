9e2526da9b738c29ccda56038a53b8db5fe4d91d
CK Pantelides <ckpantelides@gmail.com>
2020-02-20 12:40:12 +0000

    Re-commented routes, deleted deprecated code

--- index.js
+++ index.js
@@ -40,30 +40,37 @@ const port = process.env.PORT || 8000;
 app.listen(port);
 console.log('Server running on port ' + port);
 
+// pg is the module used for node to interact with postgresql
 let pg = require('pg');
 if (process.env.DATABASE_URL) {
   pg.defaults.ssl = true;
 }
 
+// pool is used instead of client (client kept returning errors)
+// c.f. pg documentation that recommends pools. No need to call pool.end() -
+// can leave pool open instead
 let connString = process.env.DATABASE_URL;
-
 const { Pool } = require('pg');
-
 const pool = new Pool({
   connectionString: connString
 });
 
+// this route confirms the backend is running, and displays a simply message
+// at the url (from index.html)
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-// when data received from client-side via axios
+// this route is called when a booking enquiry is made from the front-end
 app.post('/post', function(req, res) {
-  // body-parser saves incoming data in req.body
+  // booking enquiry saved to 'data' variable
+  // (body-parser saves incoming data in req.body)
   const data = req.body.inputs;
 
+  // get today's date i.e. the date of the enquiry
   let enquirydate = new Date().toISOString().slice(0, 10);
 
+  // insert the booking enquiry into the requests table
   pool
     .query(
       'INSERT INTO requests(enquirydate, name, email, telephone, dates, package, message, read)VALUES($1, $2, $3, $4, $5, $6, $7, $8)',
@@ -84,30 +91,9 @@ app.post('/post', function(req, res) {
         throw err;
       })
     );
-
-  // pool.end();
-
-  // Below inserts data but throws error message
-  /*
-  pool.query(
-    'INSERT INTO requests(name,email,message)VALUES($1,$2,$3)',
-    [data.name, data.email, data.message],
-    err => {
-      console.log(err);
-      pool.end();
-    }
-  );
-  */
-  // The below is confirmed as working
-  /*
-  pool.query("INSERT INTO requests(name)VALUES('Mary Ann')", (err, res) => {
-    console.log(err, res);
-    pool.end();
-  });
-  */
 });
 
-// show booking enquiries to admin
+// this route is called to show the booking enquiries to the admin on the frontend
 app.get('/enquiries', function(req, res, callback) {
   pool.query(
     'SELECT rowid, enquirydate, name, email, telephone, dates, package, message, read FROM requests',
@@ -115,7 +101,6 @@ app.get('/enquiries', function(req, res, callback) {
       if (err) {
         return console.log(err.message);
       } else {
-        //console.log(res.rows);
         return callback(res.rows);
       }
     }
@@ -123,55 +108,21 @@ app.get('/enquiries', function(req, res, callback) {
   function callback(data) {
     res.send(data);
   }
-
-  // Below confirmed as working. N/B reading from booking not requests
-  /*
-  const { Client } = require('pg');
-
-  const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-  });
-
-  client
-    .connect()
-    .then(() => {
-      console.log('connected');
-      retrieveEnquiries();
-    })
-    .catch(e => {
-      console.log(e);
-    });
-
-  function retrieveEnquiries() {
-    client.query(
-      'SELECT rowid,enquirydate, name, email, telephone, dates, package, message, read FROM bookings',
-      (err, res) => {
-        if (err) {
-          return console.log(err.message);
-        } else {
-          console.log(res.rows);
-          return callback(res.rows);
-          client.end();
-        }
-      }
-    );
-    function callback(data) {
-      res.send(data);
-    }
-  }
-  */
 });
 
+// this route is called when enquiries are deleted, marked as "read" or reordered
+// by the admin on the frontend
 app.post('/update-enquiries', function(req, res) {
+  // set data to the updated enquiries received from the frontend
   const data = req.body.data;
 
-  // iterate over updated enquiry data and input into bookings table
+  // iterate over the updated enquiry data and insert into requests table
   function updateEnquiries() {
     data.forEach(function(el, index) {
+      // rowid is reset to account for enquiries being re-ordered by the user
       let rowid = index + 1;
 
-      // insert updated enquiry data into bookings table
+      // insert updated enquiry data into requests table
       pool
         .query(
           'INSERT INTO requests (rowid, enquirydate, name, email, telephone, dates, package, message, read) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
@@ -187,7 +138,7 @@ app.post('/update-enquiries', function(req, res) {
             el.read
           ]
         )
-        .then(console.log('Updates inserted into detabase'))
+        .then(console.log('Updates inserted into database'))
         .catch(err =>
           setImmediate(() => {
             throw err;
@@ -196,7 +147,8 @@ app.post('/update-enquiries', function(req, res) {
     });
   }
 
-  // delete all rows from requests table
+  // deletes all rows from the requests table and then calls updateEnquiries()
+  // this is necessary to reset the rowids, to account for reodered enquiries
   pool.query('DELETE FROM requests', function(err) {
     if (err) {
       return console.error(err.message);
@@ -206,132 +158,4 @@ app.post('/update-enquiries', function(req, res) {
   });
 });
 
-/*
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
-
-// when data received from client-side via axios
-app.post("/post", function(req, res) {
-  // body-parser saves incoming data in req.body
-  const data = req.body.inputs;
-
-  // open database
-  let db = new sqlite3.Database("data.db", err => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
-
-  let enquirydate = new Date().toISOString().slice(0, 10);
-  let name = data.name;
-  let email = data.email;
-  let telephone = data.telephone;
-  let dates = data.dates;
-  let package = data.package;
-  let message = data.message;
-  let read = "false";
-
-  // insert form input into table
-  db.run(
-    "INSERT INTO bookings (enquirydate, name, email, telephone, dates, package, message, read) VALUES (?,?,?,?,?,?,?,?)",
-    enquirydate,
-    name,
-    email,
-    telephone,
-    dates,
-    package,
-    message,
-    read
-  );
-
-  // close the database connection
-  db.close(err => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
-});
-
-// show booking enquiries to admin
-app.get("/enquiries", function(req, res, callback) {
-  // open database connection
-  let db = new sqlite3.Database("data.db", "OPEN_READONLY", err => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
-
-  db.all(
-    "SELECT rowid,enquirydate, name, email, telephone, dates, package, message, read FROM bookings",
-    function(err, rows) {
-      if (err) {
-        return console.log(err.message);
-      } else {
-        return callback(rows);
-        db.close();
-      }
-    }
-  );
-  function callback(data) {
-    res.send(data);
-  }
-});
-
-app.post("/update-enquiries", function(req, res) {
-  const data = req.body.data;
-
-  // iterate over updated enquiry data and input into bookings table
-  function updateEnquiries() {
-    data.forEach(function(el, index) {
-      let rowid = index + 1;
-      let enquirydate = el.enquirydate;
-      let name = el.name;
-      let email = el.email;
-      let telephone = el.telephone;
-      let dates = el.dates;
-      let package = el.package;
-      let message = el.message;
-      let read = el.read;
-
-      // insert updated enquiry data into bookings table
-      db.run(
-        "INSERT INTO bookings (rowid,enquirydate, name, email, telephone, dates, package, message, read) VALUES (?,?,?,?,?,?,?,?,?)",
-        rowid,
-        enquirydate,
-        name,
-        email,
-        telephone,
-        dates,
-        package,
-        message,
-        read
-      );
-    });
-  }
-
-  // open database
-  let db = new sqlite3.Database("data.db", err => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
-
-  // delete all rows from bookings table
-  db.run("DELETE FROM bookings", function(err) {
-    if (err) {
-      return console.error(err.message);
-    } else {
-      updateEnquiries();
-    }
-  });
-
-  // close the database connection
-  db.close(err => {
-    if (err) {
-      return console.error(err.message);
-    }
-  });
-});
-*/
 module.exports = app;
